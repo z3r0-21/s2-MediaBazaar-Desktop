9e2198a0e25f0f6b0985bcbcd1fd6ddb8d4fa4e4
Dimitar Prisadnikov <d.prisadnikov@student.fontys.nl>
2021-06-16 16:22:34 +0200

    HLR front end changes

--- MediaBazaarApp/MediaBazaarApp/AdministrationForm.Designer.cs
+++ MediaBazaarApp/MediaBazaarApp/AdministrationForm.Designer.cs
@@ -243,6 +243,8 @@ namespace MediaBazaarApp
             this.dgvHLR = new System.Windows.Forms.DataGridView();
             this.HolidayRequestsTab = new System.Windows.Forms.TabPage();
             this.cbHLRselection = new System.Windows.Forms.ComboBox();
+            this.lbHLR = new System.Windows.Forms.Label();
+            this.lblHLRstatus = new System.Windows.Forms.Label();
             this.ManageDepartmentsTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDepartments)).BeginInit();
             this.gbxSearchDep.SuspendLayout();
@@ -2074,6 +2076,7 @@ namespace MediaBazaarApp
             // 
             // dgvEmployees
             // 
+            this.dgvEmployees.BackgroundColor = System.Drawing.Color.WhiteSmoke;
             this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvEmployees.Location = new System.Drawing.Point(35, 176);
             this.dgvEmployees.Name = "dgvEmployees";
@@ -2163,7 +2166,7 @@ namespace MediaBazaarApp
             // 
             // btnRemoveEmp
             // 
-            this.btnRemoveEmp.BackColor = System.Drawing.Color.Coral;
+            this.btnRemoveEmp.BackColor = System.Drawing.Color.LightSalmon;
             this.btnRemoveEmp.Location = new System.Drawing.Point(814, 579);
             this.btnRemoveEmp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnRemoveEmp.Name = "btnRemoveEmp";
@@ -2568,7 +2571,7 @@ namespace MediaBazaarApp
             // 
             // btnDeclineEditAccountRequest
             // 
-            this.btnDeclineEditAccountRequest.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnDeclineEditAccountRequest.BackColor = System.Drawing.Color.OrangeRed;
             this.btnDeclineEditAccountRequest.Location = new System.Drawing.Point(769, 553);
             this.btnDeclineEditAccountRequest.Name = "btnDeclineEditAccountRequest";
             this.btnDeclineEditAccountRequest.Size = new System.Drawing.Size(159, 53);
@@ -2579,8 +2582,8 @@ namespace MediaBazaarApp
             // 
             // btnAcceptEditAccountRequest
             // 
-            this.btnAcceptEditAccountRequest.BackColor = System.Drawing.Color.LightSalmon;
-            this.btnAcceptEditAccountRequest.Location = new System.Drawing.Point(552, 553);
+            this.btnAcceptEditAccountRequest.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnAcceptEditAccountRequest.Location = new System.Drawing.Point(604, 553);
             this.btnAcceptEditAccountRequest.Name = "btnAcceptEditAccountRequest";
             this.btnAcceptEditAccountRequest.Size = new System.Drawing.Size(159, 53);
             this.btnAcceptEditAccountRequest.TabIndex = 1;
@@ -2590,7 +2593,7 @@ namespace MediaBazaarApp
             // 
             // dgvEditAccountRequests
             // 
-            this.dgvEditAccountRequests.BackgroundColor = System.Drawing.Color.Honeydew;
+            this.dgvEditAccountRequests.BackgroundColor = System.Drawing.Color.WhiteSmoke;
             this.dgvEditAccountRequests.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvEditAccountRequests.Location = new System.Drawing.Point(113, 229);
             this.dgvEditAccountRequests.Name = "dgvEditAccountRequests";
@@ -2700,47 +2703,53 @@ namespace MediaBazaarApp
             // 
             // btnHolidayRequestsAccept
             // 
-            this.btnHolidayRequestsAccept.Location = new System.Drawing.Point(19, 48);
+            this.btnHolidayRequestsAccept.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnHolidayRequestsAccept.Location = new System.Drawing.Point(719, 567);
             this.btnHolidayRequestsAccept.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnHolidayRequestsAccept.Name = "btnHolidayRequestsAccept";
             this.btnHolidayRequestsAccept.Size = new System.Drawing.Size(163, 54);
             this.btnHolidayRequestsAccept.TabIndex = 6;
             this.btnHolidayRequestsAccept.Text = "Accept";
-            this.btnHolidayRequestsAccept.UseVisualStyleBackColor = true;
+            this.btnHolidayRequestsAccept.UseVisualStyleBackColor = false;
             // 
             // btnHolidayRequestsDecline
             // 
-            this.btnHolidayRequestsDecline.Location = new System.Drawing.Point(19, 110);
+            this.btnHolidayRequestsDecline.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnHolidayRequestsDecline.Location = new System.Drawing.Point(888, 567);
             this.btnHolidayRequestsDecline.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnHolidayRequestsDecline.Name = "btnHolidayRequestsDecline";
             this.btnHolidayRequestsDecline.Size = new System.Drawing.Size(163, 54);
             this.btnHolidayRequestsDecline.TabIndex = 7;
             this.btnHolidayRequestsDecline.Text = "Decline";
-            this.btnHolidayRequestsDecline.UseVisualStyleBackColor = true;
+            this.btnHolidayRequestsDecline.UseVisualStyleBackColor = false;
             // 
             // btnHolidayRequestsClearSelected
             // 
-            this.btnHolidayRequestsClearSelected.Location = new System.Drawing.Point(19, 385);
+            this.btnHolidayRequestsClearSelected.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnHolidayRequestsClearSelected.Location = new System.Drawing.Point(101, 567);
             this.btnHolidayRequestsClearSelected.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnHolidayRequestsClearSelected.Name = "btnHolidayRequestsClearSelected";
             this.btnHolidayRequestsClearSelected.Size = new System.Drawing.Size(163, 54);
             this.btnHolidayRequestsClearSelected.TabIndex = 8;
             this.btnHolidayRequestsClearSelected.Text = "Unmark selected";
-            this.btnHolidayRequestsClearSelected.UseVisualStyleBackColor = true;
+            this.btnHolidayRequestsClearSelected.UseVisualStyleBackColor = false;
             // 
             // dgvHLR
             // 
+            this.dgvHLR.BackgroundColor = System.Drawing.Color.WhiteSmoke;
             this.dgvHLR.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHLR.Location = new System.Drawing.Point(188, 48);
+            this.dgvHLR.Location = new System.Drawing.Point(82, 163);
             this.dgvHLR.Name = "dgvHLR";
             this.dgvHLR.RowHeadersWidth = 51;
             this.dgvHLR.RowTemplate.Height = 24;
-            this.dgvHLR.Size = new System.Drawing.Size(881, 480);
+            this.dgvHLR.Size = new System.Drawing.Size(969, 370);
             this.dgvHLR.TabIndex = 12;
             // 
             // HolidayRequestsTab
             // 
             this.HolidayRequestsTab.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.HolidayRequestsTab.Controls.Add(this.lblHLRstatus);
+            this.HolidayRequestsTab.Controls.Add(this.lbHLR);
             this.HolidayRequestsTab.Controls.Add(this.cbHLRselection);
             this.HolidayRequestsTab.Controls.Add(this.dgvHLR);
             this.HolidayRequestsTab.Controls.Add(this.btnHolidayRequestsClearSelected);
@@ -2755,16 +2764,37 @@ namespace MediaBazaarApp
             // 
             // cbHLRselection
             // 
+            this.cbHLRselection.BackColor = System.Drawing.Color.WhiteSmoke;
             this.cbHLRselection.FormattingEnabled = true;
             this.cbHLRselection.Items.AddRange(new object[] {
             "All",
             "In progress",
             "Accepted",
             "Declined"});
-            this.cbHLRselection.Location = new System.Drawing.Point(840, 14);
+            this.cbHLRselection.Location = new System.Drawing.Point(429, 102);
             this.cbHLRselection.Name = "cbHLRselection";
             this.cbHLRselection.Size = new System.Drawing.Size(216, 28);
             this.cbHLRselection.TabIndex = 13;
+            // 
+            // lbHLR
+            // 
+            this.lbHLR.AutoSize = true;
+            this.lbHLR.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbHLR.Location = new System.Drawing.Point(424, 43);
+            this.lbHLR.Name = "lbHLR";
+            this.lbHLR.Size = new System.Drawing.Size(227, 28);
+            this.lbHLR.TabIndex = 14;
+            this.lbHLR.Text = "Holiday leave requests\r\n";
+            // 
+            // lblHLRstatus
+            // 
+            this.lblHLRstatus.AutoSize = true;
+            this.lblHLRstatus.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblHLRstatus.Location = new System.Drawing.Point(175, 102);
+            this.lblHLRstatus.Name = "lblHLRstatus";
+            this.lblHLRstatus.Size = new System.Drawing.Size(230, 23);
+            this.lblHLRstatus.TabIndex = 15;
+            this.lblHLRstatus.Text = "Filter requests on status >>";
             // 
             // AdministrationForm
             // 
@@ -2849,6 +2879,7 @@ namespace MediaBazaarApp
             this.settingsTab.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvHLR)).EndInit();
             this.HolidayRequestsTab.ResumeLayout(false);
+            this.HolidayRequestsTab.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -3067,5 +3098,7 @@ namespace MediaBazaarApp
         private System.Windows.Forms.Button btnHolidayRequestsClearSelected;
         private System.Windows.Forms.Button btnHolidayRequestsDecline;
         private System.Windows.Forms.Button btnHolidayRequestsAccept;
+        private System.Windows.Forms.Label lblHLRstatus;
+        private System.Windows.Forms.Label lbHLR;
     }
 }
