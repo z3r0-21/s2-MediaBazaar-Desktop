d3564f4a6efb5cbb29e0b6d2c1399921cf7526e3
Dimitar Prisadnikov <d.prisadnikov@student.fontys.nl>
2021-06-16 16:05:45 +0200

    added HLR class

---
+++ MediaBazaarApp/MediaBazaarApp/HolidayLeaveRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarApp
+{
+    public class HolidayLeaveRequest
+    {
+        private Employee employee;
+        private DateTime startDate;
+        private DateTime endDate;
+        private int totalDays;
+        private string status;
+        private string comments;
+
+        
+        public Employee Employee { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public int TotalDays { get; private set; }
+        public string Status { get; private set; }
+        public string Comments { get; private set; }
+
+        /*public HolidayLeaveRequest(emp)
+        {
+            emp
+        }*/
+    }
+}
